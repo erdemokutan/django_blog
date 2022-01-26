f83a77e3be3c4061003fd1fae153595223a59bad
merminator <erdem.okutan.mdbf16@iste.edu.tr>
2022-01-26 15:00:45 +0300

    -background updated(CSS) -model file updated -database connection added(SQLite) -basic functional features added(mostly HTML pages)

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+
+
 # http://127.0.0.1:8000/      => index
 # http://127.0.0.1:8000/index => index
 # http://127.0.0.1:8000/blogs => blogs
@@ -9,10 +11,10 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index,name="home"),
     path("index", views.index),
-    path("blogs",views.blogs),
-    path("blogs/<int:id>", views.blog_details),
+    path("blogs",views.blogs,name="blogs"),
+    path("blogs/<int:id>", views.blog_details,name="blog_details"),
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+data={
+    "blogs": [
+        {
+            "id":1,
+            "title":"erdem okutan",
+            "image":"portal.png",
+            "is_active": True,
+            "is_home": False,
+            "description":"fevkaladenin fevkinde"
+        },
+{
+            "id":2,
+            "title":"beyin fukarası erdem",
+            "image":"portal.png",
+            "is_active": True,
+            "is_home": True,
+            "description":"fevkaladenin fevkinde"
+        },
+{
+            "id":3,
+            "title":"zihin züğürdü erdem",
+            "image":"portal.png",
+            "is_active": False,
+            "is_home": True,
+            "description":"fevkaladenin fevkinde"
+        }
+    ]
+}
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    context={
+        "blogs":data["blogs"]
+    }
+    return render(request, "blog/index.html",context)
 
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html",context)
 
 
 def blog_details(request, id):
+    blogs=data["blogs"]
+    selectedBlog=None
+
+    for blog in blogs:
+        if blog["id"]==id:
+            selectedBlog=blog
+
+
     return render(request, "blog/blog-details.html", {
-        "id": id
+       "blog": selectedBlog
     })
